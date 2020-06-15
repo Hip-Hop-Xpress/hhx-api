52d445e1c807c0b00aff318720e36c9b704c52d2
Renzo <renzol2@illinois.edu>
2020-06-15 13:52:48 -0500

    initial commit for Hip Hop Xpress API

---
+++ functions/index.js
@@ -0,0 +1,40 @@
+// Built initially using this guide:
+// https://medium.com/better-programming/building-an-api-with-firebase-109041721f77
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors({ origin: true}));
+
+var serviceAccount = require("./permissions.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://hhx-api-48896.firebaseio.com/"
+});
+const db = admin.firestore();
+
+// get
+app.get('/hello-world', (req, res) => {
+  return res.status(200).send('Hello World!');
+});
+
+// create
+app.post('/api/create', (req, res) => {
+  console.log(req.body);
+  console.log(typeof req.body);
+  console.log(req.body.id);
+  (async () => {
+      try {
+        await db.collection('items').doc('/' + req.body.id + '/')
+            .create({item: req.body.item});
+        return res.status(200).send();
+      } catch (error) {
+        console.log(error);
+        return res.status(500).send(error);
+      }
+    })();
+});
+
+exports.app = functions.https.onRequest(app);
