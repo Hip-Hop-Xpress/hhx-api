606e7467612fa4b473cfea16d0c0e1ef2f8ec74d
Renzo <renzol2@illinois.edu>
2020-06-23 17:28:53 -0500

    remove tutorial code and add handling for unimplemented routes

--- functions/index.js
+++ functions/index.js
@@ -14,7 +14,6 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://hhx-api-48896.firebaseio.com/"
 });
-const db = admin.firestore();
 
 /**
  * Hip Hop Xpress Endpoint Imports
@@ -36,106 +35,16 @@ app.get('/alive', (req, res) => {
 });
 
 /**
- * Hip Hop Xpress Endpoints
+ * Hip Hop Xpress Endpoint routes
  */
 app.use('/variations', variations);
 
-/**
- * CRUD Operations
- */
-
-// POST create
-app.post('/api/create', (req, res) => {
-  console.log(req.body);
-  console.log(typeof req.body);
-  console.log(req.body.id);
-  (async () => {
-      try {
-        await db.collection('items').doc('/' + req.body.id + '/')
-            .create({item: req.body.item});
-        return res.status(200).send();
-      } catch (error) {
-        console.log(error);
-        return res.status(500).send(error);
-      }
-    })();
-});
-
-// GET read item
-app.get('/api/read/:item_id', (req, res) => {
-  (async () => {
-    try {
-      const document = db.collection('items').doc(req.params.item_id);
-      let item = await document.get();
-      let response = item.data();
-      return res.status(200).send(response);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(error);
-    }
-  })();
-});
-
-// GET read all
-app.get('/api/read', (req, res) => {
-  (async () => {
-    try {
-      let query = db.collection('items');
-      let response = [];
-
-      await query.get().then(querySnapshot => {
-        let docs = querySnapshot.docs;
-        
-        // Hopefully this still works even with linting error...
-        // eslint-disable-next-line promise/always-return
-        for (let doc of docs) {
-          const selectedItem = {
-            id: doc.id,
-            item: doc.data().item
-          };
-
-          response.push(selectedItem);
-        }
-
-        
-      });
-
-      return res.status(200).send(response);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(error);
-    }
-  })();
-});
-
-// PUT Update
-app.put('/api/update/:item_id', (req, res) => {
-  (async () => {
-    try {
-      const document = db.collection('items').doc(req.params.item_id);
-      await document.update({
-        item: req.body.item
-      });
-      return res.status(200).send();
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(error);
-    }
-  })();
-});
-
-// DELETE delete
-app.delete('/api/delete/:item_id', (req, res) => {
-  (async () => {
-    try {
-      const document = db.collection('items').doc(req.params.item_id);
-      await document.delete();
-      return res.status(200).send();
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(error);
-    }
-  })();
+// For unhandled routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`
+  });
 });
 
 exports.app = functions.https.onRequest(app);
