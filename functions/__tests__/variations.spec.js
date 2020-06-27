0db10912610291fd42320ffb9edaf9272db6d804
Renzo <renzol2@illinois.edu>
2020-06-27 18:01:13 -0500

    clean whitespace and comments

--- functions/__tests__/variations.spec.js
+++ functions/__tests__/variations.spec.js
@@ -4,18 +4,14 @@ const supertest = require('supertest');
 const app = require('../server');
 
 describe('Testing the variations API', () => {
-
-  it('tests the jest framework if it works', () => {
-    expect(4).toBe(4);
-  });
-
-  it('tests the /alive route to verify connection to local server', async () => {
+  it('tries connecting to test endpoints to verify connection', async () => {
     const response = await supertest(app).get('/alive');
 
     expect(response.status).toBe(200);
     expect(response.text).toEqual('The Hip Hop Xpress API is alive!');
   });
 
+  // Required to close connection to server once tests are done
   afterAll(done => {
     app.close();
     done();
--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,7 @@ const admin = require('firebase-admin');
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app.use(cors({ origin: true}));
+app.use(cors({ origin: true }));
 
 var serviceAccount = require('./permissions.json');
 
