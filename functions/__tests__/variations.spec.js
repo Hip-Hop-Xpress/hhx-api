61fed337b5de4af94c451385ddfee465d92980d3
Renzo <renzol2@illinois.edu>
2020-06-27 17:24:17 -0500

    successfully integrate jest

--- functions/__tests__/variations.spec.js
+++ functions/__tests__/variations.spec.js
@@ -1,1 +1,11 @@
 // Test file for variations endpoints
+
+describe('Testing the variations API', () => {
+
+  it('tests the jest framework if it works', () => {
+
+    expect(4).toBe(4);
+
+  });
+
+});
