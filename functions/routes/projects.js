32490e1f2b5a5c9435b565b1c521ce26aad8e06a
Renzo <renzol2@illinois.edu>
2020-07-04 23:45:01 -0500

    add plan for projects endpoints

--- functions/routes/projects.js
+++ functions/routes/projects.js
@@ -8,6 +8,15 @@ const db = admin.firestore();
 const httpCodes = require('../errors/codes');
 const errorTypes = require('../errors/types');
 
-// Implementation goes here
+/**
+ * Projects endpoints
+ * 
+ *   POST, GET               /projects
+ *         GET, PUT, DELETE  /projects/:id
+ *   POST, GET               /projects/:id/members
+ *   POST, GET               /projects/:id/description
+ * 
+ * TODO: add support for query strings eventually
+ */
 
 module.exports = routes;
