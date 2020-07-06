5fe890f139db5d93f93493f18760af7ded5ccf23
Renzo <renzol2@illinois.edu>
2020-07-06 14:09:01 -0500

    implement GET and POST root functions

--- functions/routes/projects.js
+++ functions/routes/projects.js
@@ -5,8 +5,17 @@ const routes = require('express').Router();
 const admin = require('firebase-admin');
 const db = admin.firestore();
 
-const httpCodes = require('../errors/codes');
-const errorTypes = require('../errors/types');
+// Error handling functions/constants
+const {
+  sendNonexistentIdError,
+  sendIncorrectTypeError,
+  sendSchemaValidationError,
+} = require('../errors/helpers');
+
+const { OK } = require('../errors/codes');
+
+// Collection name in Firestore
+const collection = 'projects';
 
 /**
  * Schematics for Project data
@@ -15,7 +24,7 @@ const projectString      = Joi.string().min(1);  // general non-empty string
 const projectId          = Joi.number().integer().min(0);
 const projectName        = projectString;
 const projectStartDate   = Joi.string().min(4);
-const projectEndDate     = Joi.string().min(4);
+const projectEndDate     = Joi.string().min(4).allow(null);
 const projectDescription = Joi.array().min(1).items(projectString.required());
 const projectMembers     = Joi.array().min(1).items(projectString.required());
 const projectIcon        = projectString;
@@ -31,7 +40,7 @@ const postSchema = Joi.object({
   icon:        projectIcon.required()
 });
 
-// PUT /variations/:id schema
+// PUT /projects/:id schema
 const putSchema = Joi.object({
   id:          projectId,
   name:        projectName,
@@ -46,11 +55,77 @@ const putSchema = Joi.object({
  * Projects endpoints
  * 
  *   POST, GET               /projects
- *         GET, PUT, DELETE  /projects/:id
- *   POST, GET               /projects/:id/members
- *   POST, GET               /projects/:id/description
+ *         GET, PUT, DELETE  /projects/:id TODO:
+ *   POST, GET               /projects/:id/members TODO:
+ *   POST, GET               /projects/:id/description TODO:
  * 
  * TODO: add support for query strings eventually
  */
+
+ /**
+  * POST /projects
+  */
+routes.post('/', (req, res) => {
+  (async () => {
+
+    // Validate request body using schema
+    try {
+      await postSchema.validateAsync(req.body);
+    } catch (e) {
+      return sendSchemaValidationError(res, e);
+    }
+
+    await db.collection(collection).doc(`/${req.body.id}/`)
+      .create({
+        id: req.body.id,
+        name: req.body.name,
+        description: req.body.description,
+        members: req.body.members,
+        startDate: req.body.startDate,
+        endDate: req.body.endDate,
+        icon: req.body.icon
+      });
+
+    return res.status(OK).send(req.body);
+
+  })();
+});
+
+/**
+ * GET /projects
+ */
+routes.get('/', (req, res) => {
+  (async () => {
+
+    // Query the collection and setup response
+    let query = db.collection(collection);
+    let response = [];
+
+    // Get all documents from collection
+    await query.get().then(snapshot => {
+      let docs = snapshot.docs;
+
+      for (let project of docs) {
+        // Insert all data from server doc to response doc
+        const selectedItem = {
+          id: project.id,
+          name: project.data().name,
+          description: project.data().description,
+          members: project.data().members,
+          startDate: project.data().startDate,
+          endDate: project.data().endDate,
+          icon: project.data().icon
+        };
+
+        // Put the response doc into the response list
+        response.push(selectedItem);
+      }
+
+      // Send the response once every doc has been put in
+      return res.status(OK).send(response);
+    });
+
+  })();
+});
 
 module.exports = routes;
