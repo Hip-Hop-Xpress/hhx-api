7a34b813084a6bd7b9b4c3f13732146f8ccd5022
Renzo <renzol2@illinois.edu>
2020-07-12 18:09:41 -0500

    write GET and PUT endpoint tests for location

--- functions/__tests__/location.spec.js
+++ functions/__tests__/location.spec.js
@@ -27,25 +27,101 @@ afterAll(async () => {
   functionsTest.cleanup();
 });
 
+/**
+ * Tests the single GET endpoint for location
+ */
 describe('GET endpoint', () => {
 
   it('GET /v1/location', async () => {
+
+    const res = await supertest(api).get(base);
+    expect(res.status).toBe(OK);
+
+    // Verify contents of location
+    const location = res.body;
+    
+    // Check name
+    expect(typeof location.name).toBe('string');
+    expect(location.name.length).toBeGreaterThan(0);
+    
+    // Check latitude/longitude
+    expect(typeof location.latitude).toBe('number');
+    expect(typeof location.longitude).toBe('number');
 
   });
 
 });
 
+/**
+ * Since there is only one location document, we have to use that
+ * document to test
+ * Test procedure will go as follows:
+ *   1. update the location document and test accordingly
+ *   2. after all the tests, revert the location back to original
+ */
 describe('PUT /v1/location', () => {
 
+  const originalLocation = {
+    name: 'University of Illinois at Urbana-Champaign Campustown',
+    latitude: 40.102,
+    longitude: -88.2272
+  };
+
+  const updatedLocation = {
+    name: 'City of Chicago',
+    latitude: 41.8781,
+    longitude: 87.6298
+  };
+
+  // Replace location doc after tests with original location
+  afterAll(async () => {
+
+    await supertest(api)
+      .put(base)
+      .set('Accept', /json/)
+      .send(originalLocation);
+
+  });
+
   it('updates name correctly', async () => {
+
+    const res = await supertest(api)
+      .put(base)
+      .set('Accept', /json/)
+      .send({
+        name: updatedLocation.name
+      });
+
+    expect(res.status).toBe(OK);
+    expect(res.body.name).toEqual(updatedLocation.name);
 
   });
 
   it('updates latitude correctly', async () => {
 
+    const res = await supertest(api)
+      .put(base)
+      .set('Accept', /json/)
+      .send({
+        latitude: updatedLocation.latitude
+      });
+
+    expect(res.status).toBe(OK);
+    expect(res.body.latitude).toEqual(updatedLocation.latitude);
+
   });
 
   it('updates longitude correctly', async () => {
+
+    const res = await supertest(api)
+      .put(base)
+      .set('Accept', /json/)
+      .send({
+        longitude: updatedLocation.longitude
+      });
+
+    expect(res.status).toBe(OK);
+    expect(res.body.longitude).toEqual(updatedLocation.longitude);
 
   });
 
