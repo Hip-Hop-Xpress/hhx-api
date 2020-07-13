4b351a0d527c7179e523a47c5c0b551f71e28447
Renzo <renzol2@illinois.edu>
2020-07-13 17:48:46 -0500

    update starter code with the wrap middleware fn

--- functions/routes/starter.js
+++ functions/routes/starter.js
@@ -12,6 +12,10 @@ const {
   sendSchemaValidationError,
 } = require('../errors/helpers');
 
+// This is a wrapper function that moves errors to a middleware function in app.js
+const wrap = require('../errors/wrap');
+
+// HTTP Responses (so we don't use magic numbers)
 const { OK } = require('../errors/codes');
 
 // TODO: Collection/doc name in Firestore
@@ -38,6 +42,20 @@ const putSchema = Joi.object({
  * TODO: plan out your endpoints here!
  */
 
-// TODO: write your endpoints here!
+/**
+ * TODO: write your endpoints here! Below is an example
+ * 
+ * NOTE: DO NOT FORGET THE wrap()
+ * This catches errors for you that aren't normally caught
+ * HOWEVER, you can add it AFTER you write the function, because
+ *  writing it before removes intellisense
+ * So write the entire route and then put the wrap() around
+ */
+
+routes.get('/', wrap(async (req, res, next) => {
+
+  return res.status(OK).send();
+
+}));
 
 module.exports = routes;
