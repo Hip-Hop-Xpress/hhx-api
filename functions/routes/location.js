4470cf2144fa9481154e156ecf56727d19825a50
Renzo <renzol2@illinois.edu>
2020-07-13 17:50:34 -0500

    refactor location endpoint with wrap middleware

--- functions/routes/location.js
+++ functions/routes/location.js
@@ -8,6 +8,7 @@ const db = admin.firestore();
 // Error handling functions/constants
 const { sendSchemaValidationError } = require('../errors/helpers');
 
+const wrap = require('../errors/wrap');
 const { OK } = require('../errors/codes');
 
 // Collection/doc name in Firestore
@@ -39,47 +40,43 @@ const locationSchema = Joi.object({
 /**
  * GET /location
  */
-routes.get('/', (req, res) => {
-  (async () => {
+routes.get('/', wrap(async (req, res) => {
     
-    const locationDoc = db.collection(collection).doc(locationId);
+  const locationDoc = db.collection(collection).doc(locationId);
 
-    await locationDoc.get().then(doc => {
-      let locationData = doc.data();
-      return res.status(OK).send(locationData);
-    });
+  await locationDoc.get().then(doc => {
+    let locationData = doc.data();
+    return res.status(OK).send(locationData);
+  });
 
-  })();
-});
+}));
 
 /**
  * PUT /location
  */
-routes.put('/', (req, res) => {
-  (async () => {
+routes.put('/', wrap(async (req, res, next) => {
 
-    // Validate schema before updating
-    try {
-      await locationSchema.validateAsync(req.body);
-    } catch (e) {
-      return sendSchemaValidationError(res, e);
-    }
+  // Validate schema before updating
+  try {
+    await locationSchema.validateAsync(req.body);
+  } catch (e) {
+    return sendSchemaValidationError(res, e);
+  }
 
-    const document = db.collection(collection).doc(locationId);
-    const docRef = document;
+  const document = db.collection(collection).doc(locationId);
+  const docRef = document;
 
-    await document.get().then(doc => {
-      docRef.update(req.body);
+  await document.get().then(doc => {
+    docRef.update(req.body);
 
-      // Spread operator to combine old data with updated data
-      // Shared fields are overwritten by rightmost object (updated data)
-      return res.status(OK).send({...doc.data(), ...req.body});
-    });
+    // Spread operator to combine old data with updated data
+    // Shared fields are overwritten by rightmost object (updated data)
+    return res.status(OK).send({...doc.data(), ...req.body});
+  });
 
-    // linting purposes
-    return null;
+  // linting purposes
+  return null;
 
-  })();
-});
+}));
 
 module.exports = routes;
