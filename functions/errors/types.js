2c94ae345e65c7c5866020af1457e18337113cff
Renzo <renzol2@illinois.edu>
2020-07-14 14:43:11 -0500

    update wiki with new error type

--- functions/errors/types.js
+++ functions/errors/types.js
@@ -3,5 +3,5 @@ exports.API_ERR              = 'api_error';
 exports.INVALID_ENDPOINT_ERR = 'invalid_endpoint_error';
 exports.INVALID_REQUEST_ERR  = 'invalid_request_error';
 exports.ID_NOT_FOUND_ERR     = 'id_not_found_error';
-exports.ID_ALREADY_EXISTS    = 'id_already_exists';
+exports.ID_ALREADY_EXISTS    = 'id_already_exists_error';
 exports.RATE_LIMIT_ERR       = 'rate_limit_error';
