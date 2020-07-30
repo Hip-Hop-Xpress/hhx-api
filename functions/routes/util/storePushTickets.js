1fd8c6b0e5c3cc8d641ad330d79389b6c3816a3f
Renzo <renzol2@illinois.edu>
2020-07-30 16:49:04 -0500

    implement fn to store push tickets

--- functions/routes/util/storePushTickets.js
+++ functions/routes/util/storePushTickets.js
@@ -5,9 +5,24 @@ const db = admin.firestore();
 
 const collectionName = 'tickets';
 
-
-const storePushTickets = (formattedPushTickets) => {
-
+/**
+ * Adds formatted push tickets to Firestore
+ * @param {Array<Object>} formattedPushTickets push tickets formatted 
+ */
+const storePushTickets = async (formattedPushTickets) => {
+  try {
+    for (let pushTicketDoc of formattedPushTickets) {
+      // If doc doesn't have timestamp already, add it
+      if (pushTicketDoc.created === undefined) {
+        pushTicketDoc.created = admin.firestore.Timestamp.fromDate(new Date());
+      }
+  
+      // Put push ticket into database
+      db.collection(collectionName).add(pushTicketDoc);
+    }
+  } catch (e) {
+    console.error('Error storing push tickets', e);
+  }
 }
 
 export default storePushTickets;
